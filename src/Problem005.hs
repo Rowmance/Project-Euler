008609efa237d17b36f045332f20d4376a35721c
Roman Kolacz <roman@opengamma.com>
2018-01-01 20:46:23 +0000

    5

---
+++ src/Problem005.hs
@@ -0,0 +1,13 @@
+-- Problem 005
+-- https://projecteuler.net/problem=5
+
+-- Didn't need any code for this one, just listed the prime factors of numbers up to 20
+-- and eliminated the factors which were in common from previous numbers. 
+-- If a number had a prime factor appearing multiple times, it had to appear a minimum of that number
+-- of times.
+
+-- the resulting prime factors were as follows.
+f = foldr1 (*) [2, 3, 4, 5, 6, 7, 11, 13, 17, 19]
+
+--------------------------
+main = print f
